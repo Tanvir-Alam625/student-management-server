63fcf087ebd3269bc8351d3652843c622df91e27
tanveer <mdtanviralamht731@gmail.com>
2022-06-14 08:17:43 +0600

    DB connected

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.w2vv5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("DB Connected");
+  //   client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Student Management Server is Running");
 });
