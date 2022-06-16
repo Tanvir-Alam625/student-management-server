923be2c01ea82b4605070d5833c96310a4e4b31c
tanveer <mdtanviralamht731@gmail.com>
2022-06-17 03:19:13 +0600

    students api added

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const express = require("express");
-const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.w2vv5.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2vv5.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("DB Connected");
-  //   client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const studentCollection = client
+      .db("students")
+      .collection("studentCollection");
+    app.get("/students", async (req, res) => {
+      const query = {};
+      const students = await studentCollection.find(query).toArray();
+      res.send(students);
+    });
+    // console.log("I am in");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Student Management Server is Running");
