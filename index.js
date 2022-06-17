774b41f47fccbd024c42c98280794679d39f375c
tanveer <mdtanviralamht731@gmail.com>
2022-06-17 16:05:15 +0600

    today all done

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2vv5.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,7 +25,21 @@ async function run() {
       const students = await studentCollection.find(query).toArray();
       res.send(students);
     });
-    // console.log("I am in");
+    app.get("/student", async (req, res) => {
+      const roll = parseInt(req.query.roll);
+      const query = { roll: roll };
+      const student = await studentCollection.findOne({ roll: roll });
+      if (student) {
+        res.send(student);
+      } else {
+        res.status(403).send({ message: "Not Found Student" });
+      }
+    });
+    app.post("/student", async (req, res) => {
+      const student = req.body;
+      const storedStudent = await studentCollection.insertOne(student);
+      res.send(storedStudent);
+    });
   } finally {
   }
 }
