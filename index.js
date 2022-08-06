361188adfe3be4a83520b0eea07a952ae8cc1782
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-08-06 15:55:05 +0600

    student delete API added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+//  mongodb URI 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2vv5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -14,17 +15,22 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// All API 
 async function run() {
   try {
     await client.connect();
     const studentCollection = client
       .db("students")
       .collection("studentCollection");
+
+    // All Student Show API 
     app.get("/students", async (req, res) => {
       const query = {};
       const students = await studentCollection.find(query).toArray();
       res.send(students);
     });
+
+    // Search Student API 
     app.get("/student", async (req, res) => {
       const roll = parseInt(req.query.roll);
       const query = { roll: roll };
@@ -35,16 +41,29 @@ async function run() {
         res.status(403).send({ message: "Not Found Student" });
       }
     });
+
+    // add  student API 
     app.post("/student", async (req, res) => {
       const student = req.body;
       const storedStudent = await studentCollection.insertOne(student);
       res.send(storedStudent);
     });
+
+    // delete Student API 
+    app.delete("/student/:id", async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: ObjectId(id)}
+      const deleteStudent = await studentCollection.deleteOne(query)
+      res.send(deleteStudent);
+
+    })
   } finally {
   }
 }
 run().catch(console.dir);
 
+
+// testing API 
 app.get("/", (req, res) => {
   res.send("Student Management Server is Running");
 });
